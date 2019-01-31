062fd99ae699862802ef97c414e607e6afc2af9f
Arthur <arthur.chevallier@etudiant.univ-rennes1.fr>
2019-01-31 12:13:14 +0100

    Test on video and app OK

--- src/app/video-component/video-component.component.ts
+++ src/app/video-component/video-component.component.ts
@@ -5,8 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './video-component.component.html',
   styleUrls: ['./video-component.component.css']
 })
-export class VideoComponentComponent implements OnInit {
+
+export class VideoComponent implements OnInit {
 	url_mp4 = "https://archive.org/download/Route_66_-_an_American_badDream/Route_66_-_an_American_badDream_512kb.mp4"
 	url_ogg = "https://archive.org/download/Route_66_-_an_American_badDream/Route_66_-_an_American_badDream.ogv"
 	constructor() { }
+	ngOnInit(){
+
+	}
 }
