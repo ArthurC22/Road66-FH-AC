51a5cbd6e499b05ba7bace81f7d949bdfe01b37d
Arthur <arthur.chevallier@etudiant.univ-rennes1.fr>
2019-01-31 11:03:07 +0100

    Link road66 video in video_component

--- src/app/video-component/video-component.component.ts
+++ src/app/video-component/video-component.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-component.component.css']
 })
 export class VideoComponentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	url_mp4 = "https://archive.org/download/Route_66_-_an_American_badDream/Route_66_-_an_American_badDream_512kb.mp4"
+	url_ogg = "https://archive.org/download/Route_66_-_an_American_badDream/Route_66_-_an_American_badDream.ogv"
+	constructor() { }
 }
